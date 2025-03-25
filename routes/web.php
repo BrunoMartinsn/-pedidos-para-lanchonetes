898ccc8dd44f6832cf11dbdc8c9be19cd43f2e54
bruno <brurro67@gmail.com>
2025-03-25 15:45:51 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Login::class);
+
+Route::get('/admin', function () {
+    return 'login admin';
+})->middleware(['auth', 'role:admin'])->name('admin.deshboard');
+
+Route::get('/user', function(){
+    return 'login user';
+
+})->middleware(['auth', 'role:user'])->name('user.deshboard');
